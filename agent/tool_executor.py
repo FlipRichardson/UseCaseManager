2099b0164ea9c589c7fceb51de70226366f05f03
Philipp <philipp_kunze@mailbox.org>
2026-02-16 11:24:06 +0100

    Updated tool executor: - holds global variable: current_user - additionally passes the current user to all functions by adding it to the arguments -> service functions will get it this way

--- agent/tool_executor.py
+++ agent/tool_executor.py
@@ -7,6 +7,9 @@ from services import UseCaseService
 # init service
 service = UseCaseService()
 
+# Global variable to track current user (set by UI)
+current_user = None
+
 # mapping
 # Map function names to actual Python functions
 tool_functions = {
@@ -26,6 +29,25 @@ tool_functions = {
     "create_person": service.create_person,                    
     "add_persons_to_use_case": service.add_persons_to_use_case 
 }
+
+
+def set_current_user(user):
+    """
+    Set the current user for permission checks.
+    Should be called by the UI after login.
+    
+    Args:
+        user (dict): User dict with id, email, role, name
+    """
+    global current_user
+    current_user = user
+
+
+def get_current_user():
+    """Get the current logged-in user."""
+    return current_user
+
+
 def execute_tool(function_name : str, arguments : dict):
     """
     Execute a tool function by name with given arguments.
@@ -44,6 +66,10 @@ def execute_tool(function_name : str, arguments : dict):
 
     try: 
         actual_function = tool_functions[function_name]
+
+        # Add current_user to arguments for all service methods
+        # (All service methods now accept current_user parameter)
+        arguments['current_user'] = current_user
 
         # call the function
         result = actual_function(**arguments)
